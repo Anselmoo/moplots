db7c7676c3721d0bfe143761d6fd926802bbfd88
Anselm Hahn <Anselm.Hahn@gmail.com>
2023-10-08 22:41:49 +0200

    Initial commit

---
+++ moplots/__init__.py
@@ -0,0 +1,6 @@
+"""Molecular Orbital Plots as Batch Series for ORCA."""
+from typing import LiteralString
+
+__moplots__ = "moplots"
+__version__ = "0.1.0"
+__repo__: LiteralString = f"https://github.com/Anselmoo/{__moplots__}"
